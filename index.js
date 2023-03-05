edb2237e3a6871ed0f9659e1c59d694d648afd66
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2023-03-05 22:48:09 +0530

    Scoring buttons done

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-const addOne = document.getElementById('add-one')
+let homeScore = document.getElementById('home-score');
+let guestScore = document.getElementById('guest-score');
+const addOneHome = document.getElementById('add-one-home');
+const addTwoHome = document.getElementById('add-two-home');
+const addThreeHome = document.getElementById('add-three-home');
+const addOneGuest = document.getElementById('add-one-guest');
+const addTwoGuest = document.getElementById('add-two-guest');
+const addThreeGuest = document.getElementById('add-three-guest');
 
-addOne.addEventListener('click', function () {
-    console.log('+1 clicked')
-})
+let homeCount = 0;
+let guestCount = 0;
+
+addOneHome.addEventListener('click', addOneH);
+
+addTwoHome.addEventListener('click', addTwoH);
+
+addThreeHome.addEventListener('click', addThreeH);
+
+addOneGuest.addEventListener('click', addOneG);
+
+addTwoGuest.addEventListener('click', addTwoG);
+
+addThreeGuest.addEventListener('click',addThreeG);
+
+function addOneH() {
+  homeCount++;
+  homeScore.innerHTML = homeCount;
+}
+function addTwoH() {
+  homeCount = homeCount + 2;
+  homeScore.innerHTML = homeCount;
+}
+
+function addThreeH() {
+  homeCount = homeCount + 3;
+  homeScore.innerHTML = homeCount;
+}
+
+function addOneG() {
+  guestCount++;
+  guestScore.innerHTML = guestCount;
+}
+
+function addTwoG() {
+  guestCount = guestCount + 2;
+  guestScore.innerHTML = guestCount;
+}
+
+function addThreeG() {
+  guestCount = guestCount + 3;
+  guestScore.innerHTML = guestCount;
+}
